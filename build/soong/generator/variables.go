c5f18f56651059ede3945ce312bb54a631621943
Ubuntu <azureuser@ubuntu.le4uofylhjdufhzuqnwfnigmlh.bx.internal.cloudapp.net>
2022-08-24 20:40:34 +0000

    vendor/hentai: hos bringup

--- build/soong/generator/variables.go
+++ build/soong/generator/variables.go
@@ -6,12 +6,12 @@ import (
 	"android/soong/android"
 )
 
-func lineageExpandVariables(ctx android.ModuleContext, in string) string {
-	lineageVars := ctx.Config().VendorConfig("lineageVarsPlugin")
+func hentaiExpandVariables(ctx android.ModuleContext, in string) string {
+	hentaiVars := ctx.Config().VendorConfig("hentaiVarsPlugin")
 
 	out, err := android.Expand(in, func(name string) (string, error) {
-		if lineageVars.IsSet(name) {
-			return lineageVars.String(name), nil
+		if hentaiVars.IsSet(name) {
+			return hentaiVars.String(name), nil
 		}
 		// This variable is not for us, restore what the original
 		// variable string will have looked like for an Expand
